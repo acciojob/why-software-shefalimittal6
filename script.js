5c92777a972f5610e0598916ff98123ee39323d5
Shefali <mittal.shefali2000@gmail.com>
2025-01-10 19:16:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-//your code here
+document.getElementById('motivation').innerHTML = 
+    "Becoming a software developer excites me because it allows me to solve real-world problems and create solutions that can have a lasting impact. " + 
+    "The ability to code gives me the power to transform ideas into reality, make meaningful contributions to technology, and continuously learn in an ever-evolving field.";
